227b203bf593dab04c844ed7bc73f849daa0f022
nsi4818 <nsi4818@students.fhv.at>
2023-04-21 10:05:37 +0200

    added comments regarding tools

--- src/main/java/at/fhv/lab2/EventBus.java
+++ src/main/java/at/fhv/lab2/EventBus.java
@@ -10,6 +10,14 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("at.fhv.lab2.eventbus")
 public class EventBus {
 
+    // für openapi docs:
+    // localhost:8080/v3/api-docs/
+
+    // für swagger ui:
+    // localhost:8080/swagger-ui/index.html
+
+    // alternative zum testen der apis: postman
+
     public static void main(String[] args) {
         SpringApplication.run(EventBus.class, args);
     }
